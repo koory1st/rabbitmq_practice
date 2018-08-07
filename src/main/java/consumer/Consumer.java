4c0799235220649a18aa562683df080f038b129d
koory1st <32436334@qq.com>
2018-08-07 17:05:40 +0800

    修改成确认消息模式

--- src/main/java/consumer/Consumer.java
+++ src/main/java/consumer/Consumer.java
@@ -15,19 +15,25 @@ public class Consumer {
         factory.setHost("127.0.0.1");
         factory.setPort(5672);
         Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+        final Channel channel = connection.createChannel();
+
+        channel.basicQos(1); // accept only one unack-ed message at a time (see below)
 
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-        DefaultConsumer consumer = new DefaultConsumer(channel) {
+        final DefaultConsumer consumer = new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope,
                                        AMQP.BasicProperties properties, byte[] body)
                     throws IOException {
                 String message = new String(body, "UTF-8");
                 System.out.println(" [x] Received '" + message + "'");
+
+                channel.basicAck(envelope.getDeliveryTag(), false);
             }
         };
-        channel.basicConsume(QUEUE_NAME, true, consumer);
+
+        boolean autoAck = false;
+        channel.basicConsume(QUEUE_NAME, autoAck, consumer);
     }
 }
